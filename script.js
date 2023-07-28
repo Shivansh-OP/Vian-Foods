538a78acd4a1f1664ea859a15d1b355ad3168b25
Shivansh-OP <shivanshsoni327@outlook.com>
2023-07-28 15:29:21 +0530

    updates

---
+++ script.js
@@ -0,0 +1,10 @@
+var showPassword = document.getElementById ("see");
+document.getElementById ("signup-login-password-button")
+function change() {
+    if (input.type === "password") {
+        input.type = "text"
+    }
+    else {
+        input.type = "password"
+    }
+}
